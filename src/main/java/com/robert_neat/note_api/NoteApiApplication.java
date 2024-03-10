07095578b797d45033b4e325de2f2e93a26a79ac
Robert Sereda <robertgoogl@gmail.com>
2024-03-10 13:39:52 +0100

    initial commit

---
+++ src/main/java/com/robert_neat/note_api/NoteApiApplication.java
@@ -0,0 +1,13 @@
+package com.robert_neat.note_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robert_neat/note_api/NoteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert_neat.note_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
